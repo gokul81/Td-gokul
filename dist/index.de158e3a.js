f46fe7e81598c9c2a0d422f49786565b31aee2ad
Gokul pradeep <gokulpradeep816@gmail.com>
2022-06-08 15:11:07 +0530

    First commit

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+let todos = [];
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const todoInput = input.value;
+    const todoObj = {
+        id: Math.random(),
+        todoName: todoInput
+    };
+    todos.push(todoObj);
+    displayTodo();
+    input.value = "";
+});
+function displayTodo() {
+    const listItem = todos.map((todo)=>{
+        return `<li class="flex justify-between border-2 rounded-sm  bg-white py-1">
+        ${todo.todoName}<button class="bg-red-200 rounded  text-red-700" onclick="deleteTodo(${todo.id})">Delete</button>
+      </li>`;
+    }).join("");
+    list.innerHTML = listItem;
+}
+function deleteTodo(id) {
+    todos = todos.filter((todo)=>{
+        return todo.id !== id;
+    });
+    displayTodo();
+}
+
+//# sourceMappingURL=index.de158e3a.js.map
